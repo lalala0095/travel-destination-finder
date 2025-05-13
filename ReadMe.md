31612e24d60b67f8cdf0969f476350edebd35e00
Lemuel Torrefiel <torrefiellemuel@gmail.com>
2025-05-13 23:24:46 +0800

    EDIT cors, ADD Readme for deployment

---
+++ ReadMe.md
@@ -0,0 +1,1 @@
+nohup uvicorn main:app --host 0.0.0.0 --port 8003 > server.log 2>&1 & echo $! > server.pid
--- main.py
+++ main.py
@@ -11,10 +11,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins = ['http://localhost:5173', 'https://travel-destination-frontend-two.vercel.app/'],
+    allow_origins = ['http://localhost:5173', 'https://travel-destination-frontend-two.vercel.app'],
     allow_credentials = True,
-    allow_methods = ['*'],
-    allow_headers = ['*']
+    allow_methods = ["*"],
+    allow_headers = ["*"]
 )
 
 @app.get("/")
